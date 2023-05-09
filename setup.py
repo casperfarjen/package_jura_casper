2e749cdafe85f1d2527d740a3566c772f439621f
Casper Farret Jentink <casper.farret@gmail.com>
2023-05-09 12:11:00 +0200

    Update setup.py
    Add more descriptions for Pypi

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="rv_curve_Casper",
     version="0.1",
@@ -5,6 +10,10 @@ setuptools.setup(
     author_email='casper.farret@gmail.com',
     description="Plot all RV curves that you want",
     packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/casperfarjen/package_jura_casper",
     install_requires=["numpy","matplotlib"],
-    classifiers=["Programming Language :: Python :: 3"],
+    classifiers=["Programming Language :: Python :: 3",
+                "Operating System :: OS Independent"],
 )
